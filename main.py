209356ef3b4a98138906f9e81f59580dc92e604a
NitroGuy10 <naviyer10@gmail.com>
2021-06-25 00:56:13 -0500

    Server and a useless canvas

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+from flask import Flask
+import render
+from markupsafe import escape
+
 print("Hello, ERS-OL!")
+
+app = Flask(__name__, static_url_path="", static_folder="static")
+lobby = {
+    "players": []
+}
+
+
+# The first player to join the lobby becomes the host
+def host():
+    lobby["players"].append(0)  # TODO temporary
+    return app.send_static_file("game.html")
+
+
+@app.route("/")
+def join():
+    if len(lobby["players"]) == 0:
+        return host()
+    lobby["players"].append(0)  # TODO temporary
+    return app.send_static_file("game.html")
+
+
+def startup():
+    render.main()
+
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=25565)
+
+
+startup()
---
+++ render.py
@@ -0,0 +1,17 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from os import walk
+
+env = Environment(
+    loader=FileSystemLoader("templates"),
+    autoescape=select_autoescape()
+)
+
+
+def main():
+    image_list = []
+    for root, dirs, files in walk("static/cards"):
+        for file in files:
+            image_list.append("cards/" + file)
+
+    with open("static/game.html", "w") as file:
+        file.write(env.get_template("game.html.jinja").render(image_list=image_list))
