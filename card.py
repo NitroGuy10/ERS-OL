23b3d0937e4ce5fa770cd4188c25007bfa38a1d9
NitroGuy10 <naviyer10@gmail.com>
2021-06-26 01:07:34 -0500

    Skeleton for websockets implementation

---
+++ card.py
@@ -0,0 +1,10 @@
+RANK_NAME = ["NONE", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        return RANK_NAME[self.rank] + " of " + self.suit
--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 from flask import Flask
 import render
+import card
 from markupsafe import escape
 
 print("Hello, ERS-OL!")
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 lobby = {
-    "players": []
+    "players": [],
+    "center": [  # List of cards in the center stack
+        card.Card(10, "Clubs")
+    ]
 }
 
 
 # The first player to join the lobby becomes the host
 def host():
     lobby["players"].append(0)  # TODO temporary
-    return app.send_static_file("game.html")
+    return join()
 
 
 @app.route("/")
 def join():
     if len(lobby["players"]) == 0:
         return host()
-    lobby["players"].append(0)  # TODO temporary
+    new_player = {
+        "id": "TODO with websockets",
+        "name": "player",  # Prompt user to enter a name (?)
+        "hand": [
+            card.Card(1, "Spades"),
+            card.Card(12, "Hearts")
+        ]
+    }
+    lobby["players"].append(new_player)
     return app.send_static_file("game.html")
+
+
+# TODO called by player's post
+def deal(player_id):
+    # TODO deal the top card from the player's deck (if it's their turn)
+    pass
+
+
+# TODO called by player's post
+def slap(player_id):
+    # TODO slap the deck and do all that that implies
+    pass
+
+
+# TODO called by player's post
+def take(player_id):
+    # TODO make the player that won the center stack receive their cards
+    pass
 
 
 def startup():
